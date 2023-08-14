18f7599488e4f2ea630f6c0288e4bd6a9ef26717
awasthilokesh2021 <115865786+awasthilokesh2021@users.noreply.github.com>
2023-08-14 20:21:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
-  //   write your code here
+ let arr1 = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
+  // let  obj = arr1.hasOwnProperty(" ")
+
+  for (let key1 in arr1) {
+    if (key1 == key) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
 
 // Do not change the code below
-const key = prompt("Enter Key.");
-alert(hasKey(key));
+//const key = prompt("Enter Key.");
+//alert(hasKey(key));
